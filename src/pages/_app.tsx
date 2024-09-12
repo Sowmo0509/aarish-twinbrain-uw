09068fa18e63ec6d5b24c96cba87351afde69625
Sowmo0509 <sowmo0509@gmail.com>
2024-09-13 00:01:03 +0600

    initial

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import "@/styles/globals.css";
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import "@mantine/core/styles.css";
+
+import { MantineProvider } from "@mantine/core";
+import type { AppProps } from "next/app";
+import { theme } from "@/theme";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
+}
